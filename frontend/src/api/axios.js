a8bf31a25365a22a88f8738ba4d3d9e66b35b3f6
yuuuchn <tegarfahrezi07@gmail.com>
2026-07-04 00:03:12 +0700

    Update API URL to Ngrok tunnel

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://d400-114-10-98-225.ngrok-free.app/api',
 });
 
 api.interceptors.request.use((config) => {
