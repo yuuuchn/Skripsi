4bd9c6edbbf1315e852b695ce45f9c4973972d46
yuuuchn <tegarfahrezi07@gmail.com>
2026-07-04 18:36:27 +0700

    Connect

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://d868-114-10-98-225.ngrok-free.app/api',
+  baseURL: 'https://2791-114-10-98-225.ngrok-free.app/api',
 });
 
 api.interceptors.request.use((config) => {
